11159d556155b95f48e9a15f554d8054c82e430a
Erick Souza <erick.souza@dsm.com>
2020-12-23 14:26:00 -0300

    first commit

---
+++ Test.py
@@ -0,0 +1,40 @@
+# Simple print
+print('Hello World!')
+
+# mod
+print(7 % 4)
+
+# power
+print(2 ** 3)
+
+a = 10
+
+print(a)
+
+print(type(a))
+print(len('test'))
+
+test = 'abcdefghijk'
+
+print(test[2])
+
+print(test[-1])
+
+print(test[2:])
+
+print(test[::2])
+print(test[2:7:2])
+
+print('tinker'[1:4])
+
+print(test.upper())
+
+print('This is a string {}'.format('INSERTED'))
+
+print('The {} {} {} '.format('fox', 'brown','quick'))
+
+print('The {2} {1} {0} '.format('fox', 'brown','quick'))
+
+# print('The {0} {0} {0} '.format('fox', 'brown','quick'))
+
+print('The {q} {b} {f} '.format(f='fox', b='brown',q='quick'))
