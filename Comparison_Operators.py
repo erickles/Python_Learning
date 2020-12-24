6f5a7f4d0adff5f8d6d814624a165f4f8c626871
Erick Souza <erick.souza@dsm.com>
2020-12-24 17:03:32 -0300

    Learning Ifs, Elifs and elses

---
+++ Comparison_Operators.py
@@ -0,0 +1,17 @@
+print(2==2)
+
+print(2==1)
+
+print(2!=1)
+
+print(1 < 2 < 3)
+
+print(1 < 2 > 3)
+
+print(1 < 2 and 2 > 3)
+
+print(1 < 2 or 2 > 3)
+
+not(1 == 1)
+
+not 1 == 1
---
+++ If_Elif.py
@@ -0,0 +1,20 @@
+if 6 < 7:
+    print('ok')
+
+hungry = True
+
+if hungry:
+    print('Feed me')
+else:
+    print('dont feed me')
+
+loc = 'Store'
+
+if loc == 'Auto Shop':
+    print('Cars are cool!')
+elif loc == 'Bank':
+    print('Money is cool')
+elif loc == 'Store':
+    print('Welcome to the store')
+else:
+    print('I dont know much')
