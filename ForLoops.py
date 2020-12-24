ca061c50f03e275ed27765684a75ac6998120f8d
Erick Souza <erick.souza@dsm.com>
2020-12-24 17:27:40 -0300

    Learning For Loops

---
+++ ForLoops.py
@@ -0,0 +1,42 @@
+mylist = [1,2,3,4,5,6,7,8,9,10]
+
+for num in mylist:
+    print(num)
+
+for num in mylist:
+    # Check for even
+    if num % 2 == 0:
+        print(num)
+    else:
+        print(f'Odd Number: {num}')
+
+list_sum = 0
+
+for num in mylist:
+    list_sum = list_sum + num
+
+    print(list_sum)
+
+my_string = 'Hello World'
+
+for letter in my_string:
+    print(letter)
+
+tup = (1,2,3)
+
+for t in tup:
+    print(t)
+
+my_list = [(1,2), (3,4),(5,6),(7,8)]
+
+for a,b in my_list:
+    print(a)
+    print(b)
+
+d = {'k1':1,'k2':2,'k3':3}
+
+for key,value in d.items():
+    print(value)
+
+for value in d.values():
+    print(value)
